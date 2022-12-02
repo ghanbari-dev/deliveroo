6f2ddf5a4dab1eb5e19eaa12af542c81ea2895e6
ghanbari.dev <ghanbari.dev@gmail.com>
2022-12-02 04:43:04 +0330

    feat: :sparkles: add navigation and homescreen + icon package

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 import { NativeWindStyleSheet } from "nativewind";
 
@@ -9,18 +13,12 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500">Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
