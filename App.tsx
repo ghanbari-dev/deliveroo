79c958f900e4037d51ea5f78475b1336f818b9a2
ghanbari.dev <ghanbari.dev@gmail.com>
2022-12-02 04:36:43 +0330

    feat: add tailwind css config files

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
